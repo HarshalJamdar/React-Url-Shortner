1b96c9355504cfb4864ccc99a3d97eae047e15ed
HarshalJamdar <jamdarharshal@gmail.com>
2025-09-27 15:29:16 +0530

    backend integration and route changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import ShortenerPage from './ShortenerPage';
 function App() {
   return (
     <Routes>
-            {/* Redirect "/" to "/url-shortner" */}
       <Route path="/" element={<Navigate to="/url-shortner" />} />
-
-      {/* Your URL shortener page */}
       <Route path="/url-shortner" element={<ShortenerPage />} />
-
-      {/* Optional catch-all for unmatched routes */}
       <Route path="*" element={<h2>Page Not Found</h2>} />
     </Routes>
   );
