f494758a9c64ac221b4d94b9a32a3b71585052f4
HarshalJamdar <jamdarharshal@gmail.com>
2025-10-06 09:40:34 +0530

    clean up redundant code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import ShortenerPage from './ShortenerPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/url-shortner" />} />
-      <Route path="/url-shortner" element={<ShortenerPage />} />
-      <Route path="*" element={<h2>Page Not Found</h2>} />
+      <Route path='/' element={<Navigate to='/url-shortner' />} />
+      <Route path='/url-shortner' element={<ShortenerPage />} />
+      <Route path='*' element={<h2>Page Not Found</h2>} />
     </Routes>
   );
 }
